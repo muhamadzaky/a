d594ac01d411ef176508fdad6740bc9e778c7eb9
Muhamad Zaky <muhamad.zaky@otoklix.com>
2022-09-09 04:15:33 +0900

    [v2.2.2] change scrollY navbar

--- src/components/layouts/Header.js
+++ src/components/layouts/Header.js
@@ -36,7 +36,7 @@ const Header = (props) => {
   };
 
   const changeBackground = () => {
-    const scrollPosition = 800;
+    const scrollPosition = 500;
     if (window.scrollY < scrollPosition) {
       setHasWhiteNav(false);
     } else {
