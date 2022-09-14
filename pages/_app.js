58c564335b9fe5fcc317c1e1f652fe9cc71d19c7
Muhamad Zaky <muhamad.zaky@otoklix.com>
2022-09-14 23:00:58 +0900

    [v2.2.9] remove console.log

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,6 @@ import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
-    console.log(process.env.NEXT_PUBLIC_AMPLITUDE_KEY, process.env.NEXT_PUBLIC_APP_MODE);
     amplitude.getInstance().init(process.env.NEXT_PUBLIC_AMPLITUDE_KEY);
   }, []);
 
