db563a8b6338ca8a6bc64bbfd431e812666b18e0
zakydayo <sasazakizaky@gmail.com>
2024-10-10 18:11:21 +0900

    [v3.0.5] fix copywriting

--- app/page.tsx
+++ app/page.tsx
@@ -31,7 +31,7 @@ export default function Home() {
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 text-center animate-fade-in">
         <h2 className="text-sm text-zinc-500 px-2">
-          Hello👋 A Front-end Developer - Building ReactJS web application for over 5 years.
+          A Front-end Developer - Building ReactJS web application for over 5 years.
         </h2>
 
         <div className="flex justify-center items-center text-zinc-500 gap-5 mt-3">
